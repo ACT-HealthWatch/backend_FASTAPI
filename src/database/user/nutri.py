ba34a121c223317bb970c4a7ef2ccaac59ae2ca4
lavi02 <hoil.jeong@fleetune.com>
2023-09-05 17:58:43 +0900

    [FIX] DB primaryKey 관련 수정

--- src/database/user/nutri.py
+++ src/database/user/nutri.py
@@ -8,12 +8,13 @@ from src.database.conn import *
 
 class nutriTable(Base):  # 유저 테이블
     __tablename__ = 'nutrient'  # 테이블 이름
+    id = Column(INTEGER, primary_key=True, autoincrement=True)  # 인덱스
     user_id = Column(String(50), nullable=False)  # 아이디
     kcal = Column(INTEGER, nullable=False)  # 칼로리
     carbohydrate = Column(INTEGER, nullable=False)  # 탄수화물
     protein = Column(INTEGER, nullable=False)  # 단백질
     fat = Column(INTEGER, nullable=False)  # 지방
-    generated_time = Column(DateTime, primary_key=True, default=datetime.now(
+    generated_time = Column(DateTime, default=datetime.now(
         pytz.timezone('Asia/Seoul')))  # 생성시간
 
 
